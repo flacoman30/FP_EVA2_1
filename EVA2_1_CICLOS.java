793ca00fc53de2328070b37412c4a3a1ed83152b
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:25:26 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+        for(int i = 1; i <=10; i++){
+         
+         //++---->es un operador
+         //-- -----> resta una unidad
+         //+=suma N valor
+         //-=resta N valor
+         //* multiplica por N
+         // /=divide entre N
+         System.out.print(i+"-");
+         
+         }System.out.println("");
+         for(int i = 10; i >=1; i--){
+         System.out.print(i+"-");
+           
+         }
+        
+        
+        
+        
+      
+        
+    }
+    
+}
